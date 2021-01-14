09c2feb9554acc008b0af67de6e5f7491d33c7bc
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-01-13 22:38:33 -0400

    Ball

---
+++ Brick Breaker Game/Assets/Scripts/BallScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float Force;
+    public bool inPlay;
+    public Transform paddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * Force);
+            Time.timeScale = 1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+            Time.timeScale = 0;
+        }
+    }
+}
