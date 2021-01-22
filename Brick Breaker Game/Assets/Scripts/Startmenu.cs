551abf35b1e0cb1d1b1a1f0c436fd1fac2420c01
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-01-22 16:09:46 -0400

    Levels Menu v1

--- Brick Breaker Game/Assets/Scripts/Startmenu.cs
+++ Brick Breaker Game/Assets/Scripts/Startmenu.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class Startmenu : MonoBehaviour
 {
+    public GameObject LevelsPanel, Level1, Level2, Level3, Level4, Level5;
+    public bool levelsPanel;
     public Text highscore_text;
 
     void Start()
     {
+        AvailableLevels(PlayerPrefs.GetInt("HIGHSCORE"));
+
         if (PlayerPrefs.GetInt("HIGHSCORE") != 0)
         {
             highscore_text.text = "High Score: " + PlayerPrefs.GetInt("HIGHSCORE");
@@ -26,5 +30,45 @@ public class Startmenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+
+    public void LevelPanel()
+    {
+        levelsPanel = !levelsPanel;
+        LevelsPanel.SetActive(levelsPanel);
+    }
+
+    void AvailableLevels(int Score)
+    {
+        /* Level 1 = 18             18
+         * Level 2 = 66             84
+         * Level 3 = 44             128
+         * Level 4 = 66             194
+         * Level 5 = 56             250
+        */
+        if (Score > 17)
+        {
+            Level1.SetActive(true);
+        }
+
+        if (Score > 83)
+        {
+            Level2.SetActive(true);
+        }
+
+        if (Score > 127)
+        {
+            Level3.SetActive(true);
+        }
+
+        if (Score > 193)
+        {
+            Level4.SetActive(true);
+        }
+
+        if (Score > 2899)
+        {
+            Level5.SetActive(true);
+        }
     }
 }
